24b82f1b3306245c97fd8a01eecd5d685630376c
Hugo Masclet <hugo@lukso.io>
2022-02-01 13:23:31 +0100

    Redirect to LUKSO domain

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # ERC725 Tools
 
-This repo is a [NextJS](https://nextjs.org/) project which provides debugging tools for ERC725 related smart contracts.
-
-You can check the [LUKSO Docs](https://docs.lukso.tech/) for tutorials and guides.
-
-### License
-
-ERC725 Inspect is [Apache 2.0 licensed](./LICENSE).
+👉 Moved to [lukso-network/tools-erc725-inspect](https://github.com/lukso-network/tools-erc725-inspect).
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,16 @@
+import Head from 'next/head';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div className="container">
-      <div className="content">
-        <h1 className="title is-1">🛠 ERC725 Tools</h1>
-        <p>
-          This website provides debugging tools to interact with ERC725 smart
-          contracts.
-        </p>
-        <h3 className="title is-3">Built with</h3>
-        <ul>
-          <li>
-            <a
-              className="has-text-link"
-              href="https://docs.lukso.tech/tools/erc725js/getting-started"
-            >
-              erc725.js
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Head>
+        <meta
+          httpEquiv="refresh"
+          content="0; url=https://erc725-inspect.lukso.tech/"
+        />
+      </Head>
+      <div className="content">Redirecting....</div>
     </div>
   );
 };
